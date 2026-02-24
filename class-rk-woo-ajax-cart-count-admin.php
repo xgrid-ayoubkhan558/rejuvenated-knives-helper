f4038a6111f73fbafb348e041f4b20a260ea73a9
M. Ayoub Khan <muhammad.ayoub@xgrid.co>
2026-02-24 13:56:30 +0500

    added feature of cart quantity update  method

--- class-rk-woo-ajax-cart-count-admin.php
+++ class-rk-woo-ajax-cart-count-admin.php
@@ -1,24 +1,27 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-final class RK_Woo_Ajax_Cart_Count_Admin {
-	public static function init() {
-		add_action( 'admin_init', array( __CLASS__, 'register_settings' ) );
-		add_action( 'admin_menu', array( __CLASS__, 'menu' ) );
+final class RK_Woo_Ajax_Cart_Count_Admin
+{
+	public static function init()
+	{
+		add_action('admin_init', array(__CLASS__, 'register_settings'));
+		add_action('admin_menu', array(__CLASS__, 'menu'));
 	}
 
-	public static function register_settings() {
+	public static function register_settings()
+	{
 		register_setting(
 			'rk_woo_ajax_cart_count_settings',
 			'rk_woo_ajax_cart_count_min_order_value',
 			array(
 				'type' => 'number',
-				'sanitize_callback' => function ( $value ) {
-					$value = is_numeric( $value ) ? (float) $value : 0;
-					return max( 0, $value );
+				'sanitize_callback' => function ($value) {
+					$value = is_numeric($value) ? (float) $value : 0;
+					return max(0, $value);
 				},
 				'default' => 0,
 			)
@@ -29,7 +32,7 @@ final class RK_Woo_Ajax_Cart_Count_Admin {
 			'rk_woo_ajax_cart_count_attributes_only',
 			array(
 				'type' => 'boolean',
-				'sanitize_callback' => function ( $value ) {
+				'sanitize_callback' => function ($value) {
 					return $value ? 1 : 0;
 				},
 				'default' => 0,
@@ -41,55 +44,74 @@ final class RK_Woo_Ajax_Cart_Count_Admin {
 			'rk_woo_ajax_cart_count_minimum_formula',
 			array(
 				'type' => 'string',
-				'sanitize_callback' => function ( $value ) {
-					return in_array( $value, array( 'subtotal', 'total' ) ) ? $value : 'subtotal';
+				'sanitize_callback' => function ($value) {
+					return in_array($value, array('subtotal', 'total')) ? $value : 'subtotal';
 				},
 				'default' => 'subtotal',
 			)
 		);
+
+		register_setting(
+			'rk_woo_ajax_cart_count_settings',
+			'rk_overwrite_cart_quantity',
+			array(
+				'type' => 'boolean',
+				'sanitize_callback' => function ($value) {
+					return $value ? 1 : 0;
+				},
+				'default' => 0,
+			)
+		);
 	}
 
-	public static function menu() {
+	public static function menu()
+	{
 		add_submenu_page(
 			'woocommerce',
 			'RK Cart Count',
 			'RK Cart Count',
 			'manage_woocommerce',
 			'rk-woo-ajax-cart-count',
-			array( __CLASS__, 'page' )
+			array(__CLASS__, 'page')
 		);
 	}
 
-	public static function page() {
-		if ( ! current_user_can( 'manage_woocommerce' ) ) {
+	public static function page()
+	{
+		if (!current_user_can('manage_woocommerce')) {
 			return;
 		}
 
-		$min_order_value = get_option( 'rk_woo_ajax_cart_count_min_order_value', 0 );
-		$attributes_only = (int) get_option( 'rk_woo_ajax_cart_count_attributes_only', 0 );
-		$minimum_formula = get_option( 'rk_woo_ajax_cart_count_minimum_formula', 'subtotal' );
+		$min_order_value = get_option('rk_woo_ajax_cart_count_min_order_value', 0);
+		$attributes_only = (int) get_option('rk_woo_ajax_cart_count_attributes_only', 0);
+		$minimum_formula = get_option('rk_woo_ajax_cart_count_minimum_formula', 'subtotal');
+		$overwrite_quantity = (int) get_option('rk_overwrite_cart_quantity', 0);
 
 		echo '<div class="wrap">';
 		echo '<h1>RK Cart Count</h1>';
 		echo '<p>Use shortcode: <code>[WooAjaxCartCount]</code></p>';
 
 		echo '<form method="post" action="options.php">';
-		settings_fields( 'rk_woo_ajax_cart_count_settings' );
+		settings_fields('rk_woo_ajax_cart_count_settings');
 		echo '<table class="form-table" role="presentation">';
-		echo '<tr>'; 
+		echo '<tr>';
 		echo '<th scope="row"><label for="rk_woo_ajax_cart_count_min_order_value">Minimum order value</label></th>';
-		echo '<td><input name="rk_woo_ajax_cart_count_min_order_value" id="rk_woo_ajax_cart_count_min_order_value" type="number" min="0" step="0.01" value="' . esc_attr( $min_order_value ) . '" class="regular-text" /></td>';
+		echo '<td><input name="rk_woo_ajax_cart_count_min_order_value" id="rk_woo_ajax_cart_count_min_order_value" type="number" min="0" step="0.01" value="' . esc_attr($min_order_value) . '" class="regular-text" /></td>';
 		echo '</tr>';
 		echo '<tr>';
 		echo '<th scope="row"><label for="rk_woo_ajax_cart_count_minimum_formula">Minimum value formula</label></th>';
 		echo '<td><select name="rk_woo_ajax_cart_count_minimum_formula" id="rk_woo_ajax_cart_count_minimum_formula">';
-		echo '<option value="subtotal" ' . selected( 'subtotal', $minimum_formula, false ) . '>Use cart subtotal</option>';
-		echo '<option value="total" ' . selected( 'total', $minimum_formula, false ) . '>Use cart total</option>';
+		echo '<option value="subtotal" ' . selected('subtotal', $minimum_formula, false) . '>Use cart subtotal</option>';
+		echo '<option value="total" ' . selected('total', $minimum_formula, false) . '>Use cart total</option>';
 		echo '</select></td>';
 		echo '</tr>';
 		echo '<tr>';
 		echo '<th scope="row">Fragments mode</th>';
-		echo '<td><label><input name="rk_woo_ajax_cart_count_attributes_only" type="checkbox" value="1" ' . checked( 1, $attributes_only, false ) . ' /> Attributes only (do not update visible cart HTML on AJAX)</label></td>';
+		echo '<td><label><input name="rk_woo_ajax_cart_count_attributes_only" type="checkbox" value="1" ' . checked(1, $attributes_only, false) . ' /> Attributes only (do not update visible cart HTML on AJAX)</label></td>';
+		echo '</tr>';
+		echo '<tr>';
+		echo '<th scope="row">Overwrite Cart Quantity</th>';
+		echo '<td><label><input name="rk_overwrite_cart_quantity" type="checkbox" value="1" ' . checked(1, $overwrite_quantity, false) . ' /> Overwrite cart quantity with selected quantity (instead of adding)</label></td>';
 		echo '</tr>';
 		echo '</table>';
 		submit_button();
